a773e02b241e16e9b8a3a3d00814abb665205556
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2021-07-05 03:38:22 +0500

    added slugify and nodeGeocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
-const bootcampSchema = new mongoose.Schema({
+const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please add a name"],
@@ -99,4 +101,29 @@ const bootcampSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Bootcamp", bootcampSchema);
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // DO not save address in DB
+  this.address = undefined;
+  next();
+});
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
